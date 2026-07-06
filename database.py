025a13ebaa4b53e191932046adefe72f5f07f8e9
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-07-06 20:03:13 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,11 @@
+# backend/database.py
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["buddy_program"]
+
+# employees_collection = db["employees"]
+buddy_assignments_collection = db["buddy_assignments"]
+meetings_collection = db["meetings"]
+buddies_collection = db["buddies"]
+employee_list_collection = db["employees"]
