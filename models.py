6f0f4e1be7ae702427b37f9ff34138b6a72e7f53
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-07-06 20:21:00 +0530

    Add Pydantic models for Employee and Buddy management

--- models.py
+++ models.py
@@ -1,1 +1,35 @@
+from pydantic import BaseModel
 
+class Employee(BaseModel):
+    emp_id: str
+    name: str
+    email: str
+    contact: str
+
+class BuddyAssignment(BaseModel):
+    emp_id: str
+    emp_name: str
+    buddy_id: str
+    buddy_name: str
+
+class Meeting(BaseModel):
+    emp_name: str
+    buddy_name: str
+    project_manager: str
+    meeting_date: str
+    meeting_time: str
+
+class Buddy(BaseModel):
+    buddy_id: str
+    name: str
+    email: str
+
+class LoginUser(BaseModel):
+    username: str
+    password: str
+    
+class EmployeeList(BaseModel):
+        emp_id: str
+        name: str
+        email: str
+        contact: str
