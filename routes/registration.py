4c35e36e790bd9714eddfb4a3b64ea508389a8fa
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-07-06 20:29:05 +0530

    Update registration.py

--- routes/registration.py
+++ routes/registration.py
@@ -1,13 +1,1 @@
-from fastapi import APIRouter
-from models import Employee
-from database import employees_collection
 
-router = APIRouter()
-
-@router.post("/register")
-def register_employee(employee: Employee):
-    existing = employees_collection.find_one({"emp_id": employee.emp_id})
-    if existing:
-        return {"success": False, "message": "Employee ID already exists"}
-    employees_collection.insert_one(employee.dict())
-    return {"success": True, "message": "Employee registered successfully"} 
