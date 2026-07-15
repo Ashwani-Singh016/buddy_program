7f393c067e28f41c9950bca671e886ace9876205
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-07-15 17:06:50 +0530

    Update employee_list.py

--- routes/employee_list.py
+++ routes/employee_list.py
@@ -1,14 +1,34 @@
-from fastapi import APIRouter
-from database import employee_list_collection  
+from fastapi import APIRouter, HTTPException
+from database import employee_list_collection
 
 router = APIRouter()
 
+
+
+@router.post("/employees")
+def create_employee(employee: dict):
+
+    # Check duplicate Employee ID
+    existing = employee_list_collection.find_one(
+        {"emp_id": employee["emp_id"]}
+    )
+
+    if existing:
+        raise HTTPException(status_code=400, detail="Employee ID already exists")
+
+    employee_list_collection.insert_one(employee)
+
+    return {
+        "message": "Employee added successfully"
+    }
+
+
 @router.get("/employees")
-def get_employee_list():  
+def get_employee_list():
 
     employees = []
 
-    for emp in employee_list_collection.find():   
+    for emp in employee_list_collection.find():
 
         emp["_id"] = str(emp["_id"])
 
