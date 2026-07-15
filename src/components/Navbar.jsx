c641fb49ce9819e20115d3fe142130c9bd24ec29
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-07-15 16:58:20 +0530

    Enhance Navbar with logout functionality
    Added logout button to Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,60 @@
+// // src/components/Navbar.jsx
+// import { Link } from "react-router-dom";
+
+// function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <div className="container">
+//         <Link className="navbar-brand fw-bold" to="/">
+//           Buddy Program
+//         </Link>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navMenu"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navMenu">
+//           <ul className="navbar-nav ms-auto">
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/registration">Registration</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/buddy-assign">Buddy Assign</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/schedule-meeting">Schedule Meeting</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/buddy-registration">Buddy Registration</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/employee-list">Employee List</Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+
 // src/components/Navbar.jsx
+
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ onLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">
+        <Link className="navbar-brand fw-bold" to="/home">
           Buddy Program
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -16,22 +63,46 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navMenu">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto align-items-center">
             <li className="nav-item">
-              <Link className="nav-link" to="/registration">Registration</Link>
+              <Link className="nav-link" to="/registration">
+                Registration
+              </Link>
             </li>
+
             <li className="nav-item">
-              <Link className="nav-link" to="/buddy-assign">Buddy Assign</Link>
+              <Link className="nav-link" to="/buddy-assign">
+                Buddy Assign
+              </Link>
             </li>
+
             <li className="nav-item">
-              <Link className="nav-link" to="/schedule-meeting">Schedule Meeting</Link>
+              <Link className="nav-link" to="/schedule-meeting">
+                Schedule Meeting
+              </Link>
             </li>
+
             <li className="nav-item">
-              <Link className="nav-link" to="/buddy-registration">Buddy Registration</Link>
+              <Link className="nav-link" to="/buddy-registration">
+                Buddy Registration
+              </Link>
             </li>
+
             <li className="nav-item">
-              <Link className="nav-link" to="/employee-list">Employee List</Link>
+              <Link className="nav-link" to="/employee-list">
+                Employee List
+              </Link>
+            </li>
+
+            <li className="nav-item ms-3">
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
             </li>
           </ul>
         </div>
